05bfdbe67bf711b6da26f57fd20c8861f61355b2
unknown <wangfei@DST57092.cn1.global.ctrip.com>
2014-06-26 10:32:12 +0800

    1. 注释了extend方法

--- backbone.js
+++ backbone.js
@@ -1643,6 +1643,7 @@
     // The constructor function for the new subclass is either defined by you
     // (the "constructor" property in your `extend` definition), or defaulted
     // by us to simply call the parent's constructor.
+    // 是否为子类定义了constructor构造器函数，如果没有则调用父类构造器
     if (protoProps && _.has(protoProps, 'constructor')) {
       child = protoProps.constructor;
     } else {
@@ -1650,20 +1651,24 @@
     }
 
     // Add static properties to the constructor function, if supplied.
+    // 继承父类的静态方法
     _.extend(child, parent, staticProps);
 
     // Set the prototype chain to inherit from `parent`, without calling
     // `parent`'s constructor function.
+    // 继承父类的原型对象
     var Surrogate = function(){ this.constructor = child; };
     Surrogate.prototype = parent.prototype;
     child.prototype = new Surrogate;
 
     // Add prototype properties (instance properties) to the subclass,
     // if supplied.
+    // 扩展子类原型对象
     if (protoProps) _.extend(child.prototype, protoProps);
 
     // Set a convenience property in case the parent's prototype is needed
     // later.
+    // 设置一个指向父类原型对象的属性
     child.__super__ = parent.prototype;
 
     return child;
